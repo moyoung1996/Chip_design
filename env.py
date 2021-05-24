b126ab7a2517d14f711fc03d5d2180e19d3a56f6
KaitoKiddo <2603194904@qq.com>
2021-05-24 16:32:18 +0800

    update agent env play

--- env.py
+++ env.py
@@ -1,24 +1,24 @@
 import numpy as np
 import pandas as pd
 import math
-class port_set:
+class Port_set:
     def __init__(self,mother_port,mother_die_set, daughter_port,daughter_die_set):
         self.mother_port = mother_port
         self.mother_die_set = mother_die_set
         self.daughter_port = daughter_port
         self.daughter_die_set = daughter_die_set
-class env():
+class Env():
 
     HB_centre_points = np.zeros([625,2])
     HB_upper_left_points = np.zeros([625,2])
     port_set_list=[]
     
     def __init__(self):
-        self.count = 0#计数，每调用一次setp（） +1
-        self.total_reward = 0#累计reward
-        self.action_space = np.zeros(625)#记录HB是否被占用
-        self.state = (self.count, self.total_reward, self.action_space,env.port_set_list, env.HB_centre_points, env.HB_upper_left_points)
-        self.state_ = (self.count, self.total_reward, self.action_space,env.port_set_list, env.HB_centre_points, env.HB_upper_left_points)
+        self.count = 0      # 计数，每调用一次setp（） +1
+        self.total_reward = 0       # 累计reward
+        self.action_space = np.zeros(625)       # 记录HB是否被占用
+        self.state = [self.count, self.total_reward, self.action_space,env.port_set_list, env.HB_centre_points, env.HB_upper_left_points]
+        self.state_ = [self.count, self.total_reward, self.action_space,env.port_set_list, env.HB_centre_points, env.HB_upper_left_points]
         top_connect = pd.read_csv('top_conn' ,header=None, delimiter=r"\s+")
         mother_die = pd.read_csv('mother_die.port_conn.xy' ,header=None, delimiter=r"\s+")
         daughter_die = pd.read_csv('daughter_die.port_conn.xy' ,header=None, delimiter=r"\s+")
@@ -26,39 +26,39 @@ class env():
         top_connect = top_connect.values
         daughter_die = daughter_die.values
         for i in range(0,top_connect.shape[0]):
-            top_connect[i,0]= top_connect[i,0][13:]
-            top_connect[i,1]= top_connect[i,1][11:]
+            top_connect[i,0] = top_connect[i,0][13:]
+            top_connect[i,1] = top_connect[i,1][11:]
         for i in range(0,mother_die.shape[0]):
-            mother_die[i,2]= mother_die[i,2][1:]
-            mother_die[i,3]= mother_die[i,3][:-1]
+            mother_die[i,2] = mother_die[i,2][1:]
+            mother_die[i,3] = mother_die[i,3][:-1]
         for i in range(0,daughter_die.shape[0]):
-            daughter_die[i,2]= daughter_die[i,2][1:]
-            daughter_die[i,3]= daughter_die[i,3][:-1]   
+            daughter_die[i,2] = daughter_die[i,2][1:]
+            daughter_die[i,3] = daughter_die[i,3][:-1]   
         for i in range(0,top_connect[:,1].shape[0]):
             mother_die_set = np.array([['#','#','#']])
             daughter_die_set = np.array([['#','#','#']])
             for j in range(0,mother_die[:,1].shape[0]):
-                if mother_die[j,0]==top_connect[i,1]:
+                if mother_die[j,0] == top_connect[i,1]:
                     mother_die_set = np.r_[mother_die_set,[[mother_die[j,1],mother_die[j,2],mother_die[j,3]]]]
             for k in range(0,daughter_die[:,1].shape[0]):
-                if daughter_die[k,0]==top_connect[i,0]:
+                if daughter_die[k,0] == top_connect[i,0]:
                     daughter_die_set = np.r_[daughter_die_set,[[daughter_die[k,1],daughter_die[k,2],daughter_die[k,3]]]]
             if mother_die_set.shape[0] != 1:
                 mother_die_set =np.delete(mother_die_set, 0, 0) 
             if daughter_die_set.shape[0] != 1: 
                 daughter_die_set =np.delete(daughter_die_set, 0, 0) 
-            port_set1 = port_set(top_connect[i,0],mother_die_set,top_connect[i,1],daughter_die_set)
+            port_set1 = Port_set(top_connect[i,0],mother_die_set,top_connect[i,1],daughter_die_set)
             env.port_set_list.append(port_set1)
        
-        for m in range(0,625):##计算HB坐标
+        for m in range(0,625):      # 计算HB坐标
             env.HB_centre_points[m,0] = 4*(2*(m%25)+1)
             env.HB_centre_points[m,1] = 4*(2*math.floor(m/25)+1)
             env.HB_upper_left_points[m,0] = env.HB_centre_points[m,0]-1
             env.HB_upper_left_points[m,1] = env.HB_centre_points[m,1]+1
         
 
-    def step(self,action):#action 不能大于580 
-        if self.count<len(env.port_set_list) and action<len(env.port_set_list):#判断action次数
+    def step(self,action):      # action 不能大于580 
+        if self.count<len(env.port_set_list) and action<len(env.port_set_list):     # 判断action次数
             done = False
         else:
             done = True
@@ -66,17 +66,17 @@ class env():
             self.count = self.count + 1
             self.action_space[action] = 1
             self.total_reward = -self.calculate_reward(action)
-            ##累计的action次数，累积的reward，hb占用情况，port集合列表，hb中中心坐标，hb左上坐标
-            self.state_ = (self.count,self.total_reward,self.action_space,env.port_set_list,env.HB_centre_points,env.HB_upper_left_points)
+            # 累计的action次数，累积的reward，hb占用情况，port集合列表，hb中中心坐标，hb左上坐标
+            self.state_ = [self.count, self.total_reward, self.action_space, env.port_set_list, env.HB_centre_points, env.HB_upper_left_points]
             return self.state_, self.total_reward, done
-        else:    #重复了state不会有任何变化 
+        else:    # 重复了state不会有任何变化 
             return self.state_, self.total_reward, done
 
     def reset(self):
         self.count = 0
         self.total_reward = 0
         self.action_space = np.zeros(625)
-        self.state =(self.count,self.total_reward,self.action_space,env.port_set_list,env.HB_centre_points,env.HB_upper_left_points)
+        self.state = [self.count, self.total_reward, self.action_space, env.port_set_list, env.HB_centre_points, env.HB_upper_left_points]
         return self.state
         
     def is_hb_empty(self,action):
@@ -85,11 +85,11 @@ class env():
         return self.total_reward
     def get_action_space(self):
         return self.action_space
-    def calculate_reward(self,action):#计算HB和对应端口集的reward
+    def calculate_reward(self,action):      # 计算HB和对应端口集的reward
         reward = -self.total_reward
-        if env.port_set_list[action].daughter_die_set[0,0] != '#':#如果daughter找不到top对应端口，视为无连接reward为0
+        if env.port_set_list[action].daughter_die_set[0,0] != '#':      # 如果daughter找不到top对应端口，视为无连接reward为0
             daughter_die_set_x = env.port_set_list[action].daughter_die_set[:,1].astype('float')
-            daughter_die_set_y =200-env.port_set_list[action].daughter_die_set[:,2].astype('float')#翻转，daughter本地左边转化为全局坐标
+            daughter_die_set_y =200-env.port_set_list[action].daughter_die_set[:,2].astype('float')     # 翻转，daughter本地左边转化为全局坐标
             daughter_die_points=np.zeros([len(env.port_set_list[action].daughter_die_set[:,1]),2])
             daughter_die_points[:,0] = daughter_die_set_x
             daughter_die_points[:,1] = daughter_die_set_y
--- play.py
+++ play.py
@@ -1,19 +1,8 @@
-#!/usr/bin/env python
-# coding=utf-8
-'''
-Author: John
-Email: johnjim0816@gmail.com
-Date: 2021-03-22 16:18:10
-LastEditor: John
-LastEditTime: 2021-04-11 01:24:41
-Discription: 
-Environment: 
-'''
 import sys,os
 curr_path = os.path.dirname(__file__)
 parent_path=os.path.dirname(curr_path) 
 sys.path.append(parent_path) # add current terminal path to sys.path
-import gym
+from env import Env
 import numpy as np
 import torch
 import datetime
@@ -37,7 +26,7 @@ if not os.path.exists(RESULT_PATH): # 检测是否存在文件夹
 
 class PPOConfig:
     def __init__(self) -> None:
-        self.env = 'CartPole-v0'
+        self.env = Env
         self.algo = 'PPO'
         self.batch_size = 5
         self.gamma = 0.99
@@ -51,7 +40,7 @@ class PPOConfig:
         self.train_eps = 300  # max training episodes
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # check gpu
         
-def train(cfg,env,agent):
+def play(cfg,env,agent):
     best_reward = env.reward_range[0]
     rewards= []
     ma_rewards = []  # moving average rewards
@@ -68,11 +57,7 @@ def train(cfg,env,agent):
             print(state_, reward, action)
             running_steps += 1
             ep_reward += reward
-            #set action[], 记录每一个action，reward出来之后，最终push进所有的memory。
-            #if done:
-            # #for(action):
-            # #
-            agent.memory.push(state, action, prob, val, reward, done)#调整到一个episode再Push一次，并全覆盖
+            agent.memory.push(state, action, prob, val, reward, done)   # 调整到一个episode再Push一次，并全覆盖
             if running_steps % cfg.update_fre == 0:
                 agent.update()
             state = state_
@@ -84,10 +69,6 @@ def train(cfg,env,agent):
             ma_rewards.append(ep_reward)
         avg_reward = np.mean(rewards[-100:])
         if avg_reward > best_reward:
-            # PPOConfig.self.actor_lr = 0.002
-            # PPOConfig.self.actor_lr = 0.005
-            # self.actor_lr = 0.002
-            # self.critic_lr = 0.005
             best_reward = avg_reward
             agent.save(path=SAVED_MODEL_PATH)
         print('Episode:{}/{}, Reward:{:.1f}, avg reward:{:.1f}, Done:{}'.format(i_episode+1,cfg.train_eps,ep_reward,avg_reward,done))
@@ -95,13 +76,15 @@ def train(cfg,env,agent):
 
 if __name__ == '__main__':
     cfg = PPOConfig()
-    env = gym.make(cfg.env)
-    env.seed(1)
+    env = cfg.env
+
+    # 需要预处理
     state_dim = env.observation_space.shape[0]
     print(state_dim)
-    action_dim = env.action_space.n
+
+    action_dim = len(env.get_action_space)
     print(action_dim)
     agent = PPO(state_dim, action_dim, cfg)
-    rewards, ma_rewards = train(cfg, env, agent)
+    rewards, ma_rewards = play(cfg, env, agent)
     save_results(rewards, ma_rewards, tag='train', path=RESULT_PATH)
     plot_rewards(rewards, ma_rewards, tag="train", algo=cfg.algo, path=RESULT_PATH)
